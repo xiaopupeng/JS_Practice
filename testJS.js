262ce0d67989e92adb0238ea677f4c3a1649a590
xiaopupeng <xiaopu.peng@gmail.com>
2024-10-22 12:03:21 -0500

    10-22-2024 updates

--- testJS.js
+++ testJS.js
@@ -1,3 +1,11 @@
+// Program: testJS.js
+// Author: Xiaopu Peng
+// External js file for my js practice
+
+alert("welcome to JavaScript written in external file");
+console.log("welcome to JavaScript written in external file");
+document.write("<h2>welcome to JavaScript written in external file</h2>");
+
 // define function - block of code to show welcome message
 function sayHello() {
   alert("Hello All! Welcome to JavaScript!!");
@@ -10,6 +18,6 @@ function showTotal() {
   alert("Total is : " + total);
 }
 // invoke / run / call a function
-sayHello();
-showTotal();
-alert('testJS.js working');
+// sayHello();
+// showTotal();
+// alert('testJS.js working');

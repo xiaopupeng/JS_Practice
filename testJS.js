e608f5b0f0f2c4a3f270add5268eb35dcfefdd29
xiaopupeng <xiaopu.peng@gmail.com>
2024-10-22 14:05:28 -0500

    10/22/24 3rd

--- testJS.js
+++ testJS.js
@@ -15,7 +15,7 @@ function showTotal() {
   var num1 = 10;
   var num2 = 20;
   var total = num1 + num2;
-  console.log('total is:' + total)
+  console.log('total is:' + total);
   document.write('total is:' + total);
 }
 // invoke / run / call a function
@@ -25,5 +25,6 @@ alert('testJS.js working');
 
 // Writing text string inside an element
 document.getElementById('mainHeadingText').innerHTML = 'Heading Text change dynamically on run-time';
+
 var paraText = document.getElementById('mainParaText');
 paraText.innerHTML = '<strong>This Paragraphic text inserted dynamically through innerHTMLmethod.</strong>';
